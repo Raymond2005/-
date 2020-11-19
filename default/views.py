4ef834bcb4a7a933bf5bf2886d916851bf66f645
Raymond Lee <10930328@dcsh.tp.edu.tw>
2020-11-19 11:43:22 +0800

    投票主題新增,修改,刪除

--- default/views.py
+++ default/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from .models import Poll, Option
+from django.views.generic import ListView, DetailView, RedirectView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
+# def poll_list(req):
+#    polls = models.Poll.objects.all()
+#    return render(req, 'poll_list.html',{'poll_list': polls})
+
+class PollList(ListView):
+    model = Poll
+
+class PollDetail(DetailView):
+    model = Poll
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        options = Option.objects.filter(poll_id=self.kwargs['pk'])
+        ctx['option_list'] = options
+        return ctx
+
+class PollVote(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        option = Option.objects.get(id=self.kwargs['oid'])
+        option.count += 1   #opton.count = option.count + 1
+        option.save()
+        return '/poll/{}/'.format(option.poll_id)
+        # return '/poll/'+str(option.poll_id)+'/'
+
+class PollCreate(CreateView):
+    model = Poll 
+    fields = ['subject', 'description']
+    success_url = '/poll/'
+
+class PollEdit(UpdateView):
+    model = Poll
+    fields = '__all__'
+    success_url = '/poll/'
+
+class PollDelete(DeleteView):
+    model = Poll
+    success_url = '/poll'
